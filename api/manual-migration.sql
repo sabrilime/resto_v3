296fc4ecd44c67957b0154ab2039654c161c3a99
sabrilime <sabrilioui@gmail.com>
2025-08-16 18:33:16 +0200

    docker files

--- api/manual-migration.sql
+++
@@ -1,9 +0,0 @@
--- Add Google OAuth columns to users table
-ALTER TABLE "users" ADD COLUMN "googleId" character varying;
-ALTER TABLE "users" ADD COLUMN "provider" character varying NOT NULL DEFAULT 'local';
-
--- Verify the changes
-SELECT column_name, data_type, is_nullable, column_default 
-FROM information_schema.columns 
-WHERE table_name = 'users' 
-AND column_name IN ('googleId', 'provider'); 
